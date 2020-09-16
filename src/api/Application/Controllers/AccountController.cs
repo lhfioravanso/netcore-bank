83d21de01e094e7543dd9a1ac2fae4c3288ce75f
lhfioravanso <leonardohf28@gmail.com>
2020-09-16 20:33:10 -0300

    initial commit

---
+++ src/api/Application/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<int> Get()
+        {
+            return null;
+        }
+    }
+}
